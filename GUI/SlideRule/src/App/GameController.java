e76b582f78c2b37fcb643d6c635a117b9e9d034e
Osztrogonácz Dóra <osztrogonaczdora@gmail.com>
2024-05-15 10:02:43 +0200

    initial commit

--- GUI/SlideRule/src/App/GameController.java
+++ GUI/SlideRule/src/App/GameController.java
@@ -6,7 +6,9 @@ import App.View.GameView;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-
+/**
+ * The controller class for the game.
+ */
 public class GameController implements PropertyChangeListener{
 
     private Scheduler model;
@@ -18,22 +20,46 @@ public class GameController implements PropertyChangeListener{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'propertyChange'");
     }
-
+    /**
+     * Get the model of the game.
+     * @return The model of the game.
+     */
     public Scheduler GetModel(){
         return model;
     }
 
+    /**
+     * Get the current player.
+     * @return The current player.
+     */
     public Student GetCurrentPlayer(){
         return currentPlayer;
     }
-    
+    /**
+     * Enter a room in the game.
+     * @param r The room to enter.
+     */
     public void EnterRoom(Room r){}
-
+    /**
+     * Pick up an item in the room.
+     * @param r The room where the item is located.
+     * @param i The item to pick up.
+     */
     public void PickUpItem(Room r, Item i){}
-
+    /**
+     * Drop an item in the room.
+     * @param r The room where the item will be dropped.
+     * @param i The item to drop.
+     */
     public void DropItem(Room r, Item i){}
-
+    /**
+     * Use an item.
+     * @param i The item to use.
+     */
     public void UseItem(Item i){}
-
+    /**
+     * Activate a transistor.
+     * @param t The transistor to activate.
+     */
     public void ActivateTransistor(Transistor t){}
 }
