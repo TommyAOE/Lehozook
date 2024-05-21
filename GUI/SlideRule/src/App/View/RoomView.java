87d1ba0d758518e4ba58aecec4dd5a153756aaf7
Schmidt Gergő András <s.gergo.az@gmail.com>
2024-05-21 13:16:29 +0200

    goo and gas

--- GUI/SlideRule/src/App/View/RoomView.java
+++ GUI/SlideRule/src/App/View/RoomView.java
@@ -7,11 +7,9 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-public class RoomView extends JPanel implements PropertyChangeListener{
+public class RoomView extends JLayeredPane implements PropertyChangeListener{
 
     public Point point;
     private Room room;
@@ -20,9 +18,23 @@ public class RoomView extends JPanel implements PropertyChangeListener{
     private JButton button;
     private ArrayList<JLabel> doors = new ArrayList<>();
     private JLabel floor;
+    private JLabel gool;
     private JLabel darkLayer;
 
     public RoomView(Room r) {
+        floor = new JLabel();
+        floor.setIcon(new ImageIcon("resources/room/room.png"));
+        floor.setBounds(0, 0, 150, 150);
+        gool = new JLabel();
+        gool.setIcon(new ImageIcon("resources/room/goo.png"));
+        gool.setBounds(0, 0, 150, 150);
+        JLabel gasl = new JLabel();
+        gasl.setIcon(new ImageIcon("resources/room/gas.png"));
+        gasl.setBounds(0, 0, 150, 150);
+
+        add(floor, 2);
+        add(gool, 0);
+        add(gasl, 1);
         room = r;
 
     }
